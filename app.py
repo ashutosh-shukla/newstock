c0d0a53eb1254a841be27f50ff2637b0453126d1
ashutosh-shukla <121450702+ashutosh-shukla@users.noreply.github.com>
2023-10-29 16:43:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from pandas_datareader import data as pdr
 import yfinance as yfin
 import streamlit as st
 from keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler  # Added missing import
 
 start = '2010-01-01'
 end = '2023-10-20'
@@ -21,30 +22,29 @@ st.write(df.describe())
 
 st.subheader('Closing Price vs Time Chart')
 fig = plt.figure(figsize=(12, 6))
-plt.plot(df.Close)
+plt.plot(df['Close'])  # Added square brackets for 'Close'
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time Chart with 100 Moving Average')
-ma100 = df.Close.rolling(100).mean()  # Add () to call the method
+ma100 = df['Close'].rolling(100).mean()  # Added square brackets for 'Close'
 fig = plt.figure(figsize=(12, 6))
 plt.plot(ma100)
-plt.plot(df.Close)
+plt.plot(df['Close'])  # Added square brackets for 'Close'
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time Chart with 100 & 200 Moving Average')
-ma200 = df.Close.rolling(200).mean()  # Add () to call the method
+ma200 = df['Close'].rolling(200).mean()  # Added square brackets for 'Close'
 fig = plt.figure(figsize=(12, 6))
 plt.plot(ma200)
 plt.plot(ma100)
-plt.plot(df.Close)
+plt.plot(df['Close'])  # Added square brackets for 'Close'
 st.pyplot(fig)
 
 data_train = pd.DataFrame(df['Close'][0:int(len(df) * 0.80)])
-data_test = pd.DataFrame(df['Close'][int(len(df) * 0.80):int(len(df)])
+data_test = pd.DataFrame(df['Close'][int(len(df) * 0.80):int(len(df))]
 
-
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler(feature_range=(0, 1)
+# Use MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
 data_train_array = scaler.fit_transform(data_train)
 
 # Load your Keras model from the file
@@ -64,10 +64,11 @@ for i in range(100, input_data.shape[0]):
 x_test_array, y_test_array = np.array(x_test), np.array(y_test)
 y_predicted = model.predict(x_test_array)
 
-scaler = scaler.scale_
-# Now, inverse the scaling
-scale_factor = 1 / scaler[0]
+# You don't need to redefine the scaler here
+# scaler = scaler.scale_  # Remove this line
 
+# Now, inverse the scaling
+scale_factor = 1 / scaler.scale_[0]  # Use scaler.scale_
 y_predicted = y_predicted * scale_factor
 y_test_array = y_test_array * scale_factor
 
