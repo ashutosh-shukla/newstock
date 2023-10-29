0d1d9d9dc719e1b53e5c09499a2a3d7b8a5c2b4b
ashutosh-shukla <121450702+ashutosh-shukla@users.noreply.github.com>
2023-10-29 16:18:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,7 @@ st.pyplot(fig)
 
 st.subheader('Closing Price vs Time Chart with 100 Moving Average')
 ma100 = df.Close.rolling(100).mean()  # Add () to call the method
-fig = plt.figure(figsize=(12, 6))
-
+fig = plt.figure(figsize=(12, 6)
 plt.plot(ma100)
 plt.plot(df.Close)
 st.pyplot(fig)
@@ -40,38 +39,20 @@ plt.plot(ma100)
 plt.plot(df.Close)
 st.pyplot(fig)
 
-
-
-
-data_train = pd.DataFrame(df['Close'][0:int(len(df)*0.80)])
-data_test = pd.DataFrame(df['Close'][int(len(df)*0.80):int(len(df))])
+data_train = pd.DataFrame(df['Close'][0:int(len(df) * 0.80)]
+data_test = pd.DataFrame(df['Close'][int(len(df) * 0.80):int(len(df))]
 
 from sklearn.preprocessing import MinMaxScaler
-scaler=MinMaxScaler(feature_range=(0,1))
-
-data_train_array=scaler.fit_transform(data_train)
-
-
-# #splitting data into xtrain and ytrain
-# x_training=[]
-# y_training=[]
-# for i in range(100,data_train_array.shape[0]):
-#     x_training.append(data_train_array[i-100:i])
-#     y_training.append(data_train_array[i,0])
-    
-# x_training,y_training=np.array(x_training),np.array(y_training)
-
-
-#load my model
-
+scaler = MinMaxScaler(feature_range=(0, 1)
+data_train_array = scaler.fit_transform(data_train)
 
 # Load your Keras model from the file
 model = load_model('keras_model.h5')
 
-# The rest of your code
+# Continue with the rest of your code
 past_100 = data_train.tail(100)
 final_df = pd.concat([past_100, data_test], ignore_index=True)
-input_data = scaler.fit_transform(final_df)
+input_data = scaler.transform(final_df)
 
 x_test = []
 y_test = []
@@ -98,4 +79,3 @@ plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
 st.pyplot(fig2)
-
